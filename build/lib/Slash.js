978775795a4e2210aea56bd8f89274b6dd5107e1
PixelAgent007 <oskar.manhart@gmx.de>
2022-08-08 13:48:32 +0200

    Removed built files

--- build/lib/Slash.js
+++
@@ -1,6 +0,0 @@
-import { Slash } from "discordx";
-import { UsageStorage } from "./UsageStorage";
-export function SlashEx(name, options) {
-    UsageStorage.add(name, options.usage);
-    return Slash(name, options);
-}
